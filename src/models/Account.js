af0328444266f2a639fbdda63bf014e8a0dab297
caioportela <caiofabioportela@outlook.com>
2020-08-18 21:53:41 -0300

    feat(model): add Account model

---
+++ src/models/Account.js
@@ -0,0 +1,17 @@
+const { DataTypes, Model } = require('sequelize');
+
+class Account extends Model {
+  static init(sequelize) {
+    return super.init({
+      balance: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+      },
+    },{
+      sequelize,
+      tableName: 'account'
+    });
+  }
+}
+
+module.exports = Account;
