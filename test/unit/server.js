9a21cc44ef6cd42f61de066ab5ac9310f9b99912
caioportela <caiofabioportela@outlook.com>
2020-08-18 21:45:18 -0300

    feat(test): add tests to server

---
+++ test/unit/server.js
@@ -0,0 +1,21 @@
+const should = require('should/as-function');
+const logger = require('../../src/loaders/logger');
+logger.level = 'crit';
+
+describe('Server setup', () => {
+  it('Should not be exposed', () => {
+    delete require.cache[require.resolve('../../server')];
+    process.env.NODE_ENV = 'development';
+
+    const server = require('../../server');
+    should(server).be.Object();
+  });
+
+  it('Should be exposed as function', () => {
+    delete require.cache[require.resolve('../../server')];
+    process.env.NODE_ENV = 'test';
+
+    const server = require('../../server');
+    should(server).be.Function();
+  });
+});
